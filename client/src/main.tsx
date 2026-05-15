39da79e496b4491515eb3d06805821e63ebe84f1
Fristaylo <andru200408@gmail.com>
2026-05-15 05:56:41 +0300

    Refactor App component to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./main.scss";
-import App from "./App.tsx";
-import { DataStatusProvider } from "./context/DataStatusContext.tsx";
+import Footer from "./components/Footer/Footer.tsx";
+import Dashboard from "./components/Dashboard/Dashboard.tsx";
+import Header from "./components/Header/Header.tsx";
+import StaleDataWarning from "./components/StaleDataWarning/StaleDataWarning.tsx";
+import {
+    DataStatusProvider,
+    useDataStatus,
+} from "./context/DataStatusContext.tsx";
 import { DashboardSettingsProvider } from "./context/DashboardSettingsContext.tsx";
+
+function App() {
+    const { isAnyError, isAnyStale } = useDataStatus();
+
+    return (
+        <>
+            <Header />
+            <div className="app-container">
+                {(isAnyStale || isAnyError) && (
+                    <StaleDataWarning isError={isAnyError} />
+                )}
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/:domain" element={<Dashboard />} />
+                </Routes>
+            </div>
+            <Footer />
+        </>
+    );
+}
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
